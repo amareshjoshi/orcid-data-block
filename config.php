01ce5f77a3f5b4a66e8cbcc41c0a3e4d99eca4ae
Amaresh R Joshi <joshia@msu.edu>
2022-07-10 11:38:58 -0400

    Added ORCiD data code and configuration screen from old plugin.

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+// time in seconds before reloading orcid data from orcid.org
+define( 'ORCID_CACHE_TIMEOUT', 3600);
+// XSLT file to convert orcid xml to html
+define( 'ORCID_XSLT', plugin_dir_path( __FILE__ ) . 'orcid-data-all.xsl');
+// append orcid_id to this to access a users orcid data
+define( 'ORCID_URL', 'https://pub.orcid.org/v3.0/');
+
